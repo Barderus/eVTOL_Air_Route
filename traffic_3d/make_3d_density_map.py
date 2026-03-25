c34c910c6b0d37efbd96a10a7db0b64751532b76
Barderus <reis.sev@gmail.com>
2026-03-25 13:15:27 -0500

    align 3d map default input with 1s export naming

--- traffic_3d/make_3d_density_map.py
+++ traffic_3d/make_3d_density_map.py
@@ -330,7 +330,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "csv_path",
         nargs="?",
-        default=folder.parent / "opensky" / "output" / "ohare_2019-03-09_local_1min_15nm_bbox.csv",
+        default=folder.parent / "opensky" / "output" / "ohare_2019-03-09_local_1s_15nm_bbox.csv",
         type=Path,
         help="CSV file to read.",
     )
